9f1778dd958ab691bc5b97b5758b929b853976a1
Kojot999 <piotr-ankus@wp.pl>
2022-12-20 15:32:37 +0100

    feat: adding year to footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,13 +2,15 @@ import React from "react";
 import styles from "./footer.module.scss";
 import FooterBackground from "../../img/FooterBackground.png";
 
+const year = new Date().getFullYear();
+
 function Footer() {
   return (
     <div className={styles.footer}>
       <img alt="" src={FooterBackground} />
       <div>
         <h1>Encyklopedia Age of Empires II</h1>
-        <p>Przygotowana Przez Zespół EmesClick.com RAFAŁ</p>
+        <p>{year}</p>
       </div>
     </div>
   );
